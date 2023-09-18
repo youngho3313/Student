c6f1542b23859cd273c50e4b5fbc1455d5e7bce2
youngho3313 <144213471+youngho3313@users.noreply.github.com>
2023-09-18 09:46:03 +0900

    Update MallDetailController.java

--- src/main/java/com/shopping/controller/mall/MallDetailController.java
+++ src/main/java/com/shopping/controller/mall/MallDetailController.java
@@ -1,7 +1,26 @@
 package com.shopping.controller.mall;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.shopping.controller.SuperClass;
+import com.shopping.model.dao.MallDao;
 
 public class MallDetailController extends SuperClass {
-	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		super.doGet(request, response);
+		
+		if(super.loginfo == null) {
+			super.youNeededLogin();
+			return;
+		}
+		
+		MallDao dao = new MallDao();
+		try {
+			int oid = Integer.parseInt(request.getParameter("oid")) ;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
